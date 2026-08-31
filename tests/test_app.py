dde5f58cc6108d2378775033e4308d6e079c6fd1
Matan Elbaz <matanelbaz58@gmail.com>
2026-08-31 19:04:24 +0300

    Added comments to the test

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,11 +8,11 @@ def client():
         yield client
 
 def test_homepage(client):
-    # שולח בקשת GET לעמוד הבית
+    # Sends a GET request to the homepage
     response = client.get('/')
     
-    # מוודא שהבקשה הצליחה (סטטוס 200)
+    # Ensures that the request was successful (status 200)
     assert response.status_code == 200
     
-    # מוודא שהמילה 'Todo' מופיעה ב-HTML שחוזר
+    # Ensures that the word 'Todo' appears in the returned HTML
     assert b"Todo" in response.data
